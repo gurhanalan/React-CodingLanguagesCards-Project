316637a293479fd7b8e8f7821dc7433c0ffe27c8
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-05 18:36:08 +0300

    header update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,8 @@ function Header() {
     return (
         <div className={styles.header}>
             <img src={logo} alt="Logo" />
+            <h3>Language Cards</h3>
+            <p>Click the cards for details</p>
         </div>
     );
 }
