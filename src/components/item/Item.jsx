9ede35aa2b20b8e201f406ca2ca52c50195ca59c
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-05 17:29:19 +0300

    Add Item content

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 
 function Item({ card, key, className }) {
-    // const [showPic, setshowPic] = useState(true);
+    const [showPic, setshowPic] = useState(true);
 
     return (
-        <div key={key} className={className}>
+        <div
+            key={key}
+            className={className}
+            onClick={() => setshowPic(!showPic)}
+        >
             <img src={card.img} alt="card-img" />
+            <h3>{card.name}</h3>
+
+            <ul>
+                {card.options.map((option, index) => (
+                    <li key={index}>{option}</li>
+                ))}
+            </ul>
         </div>
     );
 }
